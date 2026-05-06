a93b821ea6ceff803e13e75af38e0b6fe36823be
minkyeong23 <choi777765@gmail.com>
2026-05-06 17:29:34 +0900

    260506 Array com, OOP start

--- src/com/msa4java/edu/E05Array.java
+++ src/com/msa4java/edu/E05Array.java
@@ -12,5 +12,60 @@ public class E05Array {
 
         System.out.println(Arrays.toString(arrInt));
         System.out.println(Arrays.toString(arrChar));
+
+        // 배열의 복사
+//        int[] originArr = {1, 2, 3};
+//        int[] copyArr = originArr;
+
+//        copyArr[1] = 555;
+//        System.out.println(originArr[1]);
+
+        // for문을 이용한 복사
+
+//        for (int i = 0; i < originArr.length; i++) {
+//            copyArr[i] = originArr[i];
+//        }
+//        copyArr[1] = 55;
+//        System.out.println(originArr[1]);
+
+        // System.arraycopy() 이용해서 깊은 복사
+//        int[] originArr = {1, 2, 3};
+//        int[] copyArr = new int[originArr.length * 2];
+//        System.arraycopy(originArr, 0, copyArr, 0, originArr.length);
+//        System.out.println(originArr[1]);
+
+        // Arrays.copyof() 이용해서 깊은 복사
+        int[] originArr = {1, 2, 3};
+        int[] copyArr = new int[originArr.length * 2];
+        copyArr = Arrays.copyOf(originArr, originArr.length);
+        copyArr[1] = 55;
+        System.out.println(originArr[1]);
+
+        // 다차원 배열
+        int[][] arrMD = {
+                {1, 2, 3}
+                ,{4, 5, 6}
+                ,{7, 8, 9}
+        };
+        System.out.println(arrMD[2][2]);
+
+        // 배열의 API
+        int[] arrInt1 = {1, 2, 3};
+        int[] arrInt2 = {1, 2, 3, 4};
+        // 배열의 길이 반환
+        System.out.println(arrInt1.length);
+
+        // 배열이 같은지 비교
+        System.out.println(arrInt1.equals(arrInt2));
+        System.out.println(Arrays.equals(arrInt1, arrInt2));
+
+        int[][] arrIntDM1 = {{1,2,3}, {4,5,6}};
+        int[][] arrIntDM2 = {{1,2,3}, {4,5,6}};
+        System.out.println(Arrays.deepEquals(arrIntDM1, arrIntDM2));
+
+        // 배열의 정렬
+        int[] arrSort = {5, 2, 76, 89, 1, 2};
+        Arrays.sort(arrSort);
+        System.out.println(Arrays.toString(arrSort));
     }
 }
