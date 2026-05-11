ff1f6f92f51e750b2dc046eef3d42ca29e3f8a9e
minkyeong23 <choi777765@gmail.com>
2026-05-11 09:39:00 +0900

    260508 exception ~ generics

---
+++ src/com/msa4java/edu/collection/EduArrayList.java
@@ -0,0 +1,39 @@
+package com.msa4java.edu.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EduArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(10);
+        List<String> list2 = new ArrayList<>(10);
+
+        class MyArrList {
+            private int[] arr = new int[10];
+        }
+
+        list.add(1);
+        list.add(2);
+        list.add(5);
+        list.add(4);
+
+        System.out.println(list.get(3));
+
+        // 정렬
+        Collections.sort(list);
+        System.out.println(list.get(3));
+        System.out.println(list.size());
+
+        // 향상된 for문
+        for(int i : list) {
+            System.out.println(i);
+        }
+
+        list.forEach((i) -> {
+            System.out.println(i);
+        });
+
+
+    }
+}
